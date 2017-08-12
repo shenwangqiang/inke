fff5c423b39cc17cec146cfed11653fe25ba1b02
shenwangqiang <shenwangqang88@163.com>
2017-08-12 18:08:36 +0800

    宏定义

---
+++ inke/inke/Class/Other/SWQMacros.h
@@ -0,0 +1,16 @@
+//
+//  SWQMacros.h
+//  inke
+//
+//  Created by 沈王强 on 2017/8/12.
+//  Copyright © 2017年 沈王强. All rights reserved.
+//
+
+#ifndef SWQMacros_h
+#define SWQMacros_h
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define RGB(x,y,z) [UIColor colorWithRed:(x/255) green:(y/255) blue:(z/255) alpha:1]
+
+#endif /* SWQMacros_h */
