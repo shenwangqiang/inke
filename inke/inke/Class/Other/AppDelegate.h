81366379fcd8284eb791f0fb22a1ae614638d93b
shenwangqiang <shenwangqang88@163.com>
2017-08-12 17:27:01 +0800

    1.规范文件 2.使用cocoapods导入第三方库

---
+++ inke/inke/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  inke
+//
+//  Created by 沈王强 on 2017/8/12.
+//  Copyright © 2017年 沈王强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
